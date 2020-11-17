666e58a01790f35ed13f5cbe38e84483b3eb8fbe
Norbu Sonam <nsonam1822@gmail.com>
2020-11-17 18:57:56 -0500

    added mongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,17 @@ app.use('/class', classRouter)
 app.use('/task', taskRouter)
 app.use('/user', userRouter)
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}, () => {
+  console.log('Connected to MongoDB...')
+});
+
+// Start server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`)
 })
