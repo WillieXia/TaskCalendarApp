fabcd3fd9021a1384c7abd71fd13545b397676f2
Norbu Sonam <nsonam1822@gmail.com>
2020-11-17 18:49:18 -0500

    initialized app.js with routers

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+// Module Imports
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Router imports
+const authRouter = require('./routes/auth')
+const classRouter = require('./routes/class')
+const taskRouter = require('./routes/task')
+const userRouter = require('./routes/user')
+
+// Configure .env
+require('dotenv').config()
+
+// App configuration
+const app = express()
+const PORT = process.env.PORT || 3000
+app.use('/auth', authRouter)
+app.use('/class', classRouter)
+app.use('/task', taskRouter)
+app.use('/user', userRouter)
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`)
+})
